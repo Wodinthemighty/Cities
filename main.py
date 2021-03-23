df5d2beacf40b482c109207bd1ab4a6b03ebf2bf
Odin Jacobs <odin.jacobs@lphs.school.nz>
2021-03-23 01:19:40 +0000

    Improved board generation

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-#Board initilastion
+#Board initilisation
+import os
+import time
 height=11
 width=11
 board = [[" | " for x in range(width)] for y in range(height)]
 board[height//2][width//2] = "🏙️  "
 game = True
+
+def clear_board():
+ os.system("ls")
+ time.sleep(1)
 
 #Building data, creating buildings
 class Building:
@@ -15,13 +21,14 @@ class Building:
     def place_building(self):
       board[self.locationX][self.locationY] = self.type
     def upgrade_building(self,type,lvl):
-      self.lvl +=1
+      self.lvl += 1
 #Game loop
 while game == True:
+#resetting game board
+  clear_board()
   for column in board:
     for i in column:
       print(f" {i} ", end="")
     print()
-  Factory_1 = Building('🏭',1,3,3)
-  Factory_1.place_building()
+    print(' '*110)
 
